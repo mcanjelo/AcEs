0ec76fed9933f0a240d17bf4111cb87fdf67b7ef
tj-da-skolar <tj_da_skolar@yahoo.com>
2014-04-02 10:41:09 +0800

    3.1
    Nag-fail yung una sorry

--- AcEs/js/clickable_row.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-jQuery(document).ready(function($) {
-      $(".clickableRow").click(function() {
-            window.document.location = $(this).attr("href");
-      });
-});
-});
-x
